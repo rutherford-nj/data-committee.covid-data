42dcfff86cb4139c994d473bd1b4141972bdf2ce
James Bogosian <bogosj@gmail.com>
2020-10-20 09:20:13 -0400

    Set header to recommendation in docs.
    https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-repository-dispatch-event

--- data/trigger_update.bash
+++ data/trigger_update.bash
@@ -2,7 +2,7 @@
 
 if [ -z ${GITHUB_PAT+x} ]; then echo "Set GITHUB_PAT before running."; exit 1; fi
 
-curl -H "Accept: application/vnd.github.everest-preview+json" \
+curl -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token $GITHUB_PAT" \
     --request POST \
     --data '{"event_type": "update-data"}' \
