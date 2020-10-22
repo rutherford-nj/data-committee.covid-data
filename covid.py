7ff5756b3f1130f47889ae18c300fdca8bfa4bbb
Greg Recine <greg@gregrecine.com>
2020-10-22 11:51:18 -0400

    * Totals are nomalized * SMA is moving the right way now * Sensible plots * Code cleanup
    Need to do tables (point in time stats)
    Need to pretty up plots

--- covid.py
+++ covid.py
@@ -10,87 +10,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-
-def plot_totals(data, region, fname, title, save_plt=False):
-    """ Brain dead routine to plot total cases
-
-    :param data: tuple of
-            x_data: x-axis data (date list or array)
-            y_data: y-axis data (numeric list, array or list of lists)
-    :param region: area we are plotting
-    :param fname: filename suffux
-    :param title: chart title (str)
-    :param save_plt: save to a file(T) or show to screen(F)
-    :return: nada (fix this)
-    """
-
-    x_data = data[0]
-    y_data = data[1]
-
-    fig = plt.figure(figsize=(13, 9))
-    ax = fig.add_subplot(1, 1, 1)
-
-    ax.plot(x_data, y_data)
-
-    ax.set_ylabel('# of cases', fontsize=20)
-    my_fmt = mdates.DateFormatter('%m/%d')
-    ax.xaxis.set_major_formatter(my_fmt)
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-
-    plt.title(region + ': ' + title, fontsize=24)
-    plt.grid()
-
-    if save_plt:
-        plt.savefig(fname + '_' + region + ".svg", format="svg")
-        plt.close()
-    else:
-        plt.show()
-
-
-def plot_avgs(data, region, fname, title, save_plt=False):
-    """ Plot town cases and averages
-
-    :param data: tuple of
-            x_data: data
-            y_data: the new, sma, and ewma cases
-    :param region: area we are plotting
-    :param fname: filename suffux
-    :param title: chart title (str)
-    :param save_plt: save to a file(T) or show to screen(F)
-    """
-
-    x_data = data[0]
-    y_data = data[1]
-
-    y_new = y_data[0]
-    y_sma = y_data[1]
-    y_ewma = y_data[2]
-
-    fig = plt.figure(figsize=(13, 9))
-    ax = fig.add_subplot(1, 1, 1)
-
-    ax.plot(x_data, y_new, color='gray', marker='o', fillstyle='none', ms=2, lw=0)
-    ax.plot(x_data, y_sma, 'b-')
-    ax.plot(x_data, y_ewma, 'y-')
-
-    ax.set_ylabel('# of cases', fontsize=20)
-    my_fmt = mdates.DateFormatter('%m/%d')
-    ax.xaxis.set_major_formatter(my_fmt)
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-
-    plt.title(region + ': ' + title, fontsize=24)
-    plt.legend(['Cases', '7d SMA', '14d EWMA'], fontsize=16)
-    plt.grid()
-
-    if save_plt:
-        plt.savefig(fname + '_' + region + ".svg", format="svg")
-        plt.close()
-    else:
-        plt.show()
-
-
+# pylint: disable=R0902
+# I have 8/7 instance attributes, need to refactor?
 class CovidData:
     """
     Class that gets and massages town covid data
@@ -112,6 +33,7 @@ class CovidData:
         # Set regions
         self.regions = {'US': 'United_States',
                         'NJ': 'New_Jersey',
+                        'Bergen': 'Bergen_County',
                         'Counties': 'NJ_Counties',
                         'Rutherford': 'Rutherford'}
 
@@ -135,13 +57,18 @@ class CovidData:
         # 2019 populations
         _population = {self.regions['US']: 328.2E6,
                        self.regions['NJ']: 8.882E6,
-                       self.regions['Counties']: 932202,  # Bergen County Population
+                       self.regions['Bergen']: 932202,  # Bergen County Population
                        self.regions['Rutherford']: 18303}
+
+        _population[self.regions['Counties']] = _population[self.regions['NJ']] - _population[self.regions['Bergen']]
 
         self.population = config.get('population', _population)
 
         # Get data from sources
         self.data_df_dict = self._get_data()
+
+        # Break out counties
+        # self.data_df_dict = self._breakout()
 
         # Do smoothing
         for key in self.regions:
@@ -164,13 +91,21 @@ class CovidData:
                                                                                                               drop=True)
                 _df.loc[_df['New Cases'].isna(), 'New Cases'] = _df['Total Cases']
 
-            data_df_dict[key] = _df
+                # Break out Bergen County
+                data_df_dict[self.regions['Bergen']] = _df[_df.County == 'Bergen']
+
+                # NJ ex Bergen County
+                _df = _df[_df.County != 'Bergen'].groupby('Date').sum()
+                _df.reset_index(inplace=True)
+
+            data_df_dict[key] = _df.sort_values('Date')
 
         return data_df_dict
 
     def do_smoothing(self, covid_df, region):
         """
         Do n-day SMA and m-day EWMA smoothing
+        :param region: data region
         :param covid_df: input data frame (date, total cases, new cases)
         :return: dataframe with new columns
         """
@@ -186,11 +121,14 @@ class CovidData:
         covid_df[str(sma_win) + 'd avg / 100K'] = covid_df['New Cases / 100K'].rolling(sma_win).mean()
         covid_df[str(ewma_spn) + 'd ewma / 100K'] = covid_df['New Cases / 100K'].ewm(span=ewma_spn).mean()
 
+        covid_df['Total Cases / 100K'] = covid_df['Total Cases'] * (1.0E5 / population)
+        covid_df[str(sma_win) + 'd avg Total Cases / 100K'] = covid_df['Total Cases / 100K'].rolling(sma_win).mean()
+        covid_df[str(ewma_spn) + 'd ewma Total Cases/ 100K'] = covid_df['Total Cases / 100K'].ewm(span=ewma_spn).mean()
+
         return covid_df
 
     def do_plots(self, key, config):
         """
-        :param covid_data: covid data class
         :param key: region to plot
         :param config [dict]
                 total:
@@ -200,59 +138,167 @@ class CovidData:
                 debug: save to a file(F) or show to screen(T)
         """
 
-        do_total = config.get('total', True)
-        do_avgs = config.get('averages', True)
-        do_raw = config.get('raw', True)
-        do_scaled = config.get('scaled', True)
-        debug = config.get('debug', True)
+        # do_total = config.get('total', True)
+        # do_avgs = config.get('averages', True)
+        # do_raw = config.get('raw', True)
+        # do_scaled = config.get('scaled', True)
+        self.debug = config.get('debug', True)
 
-        save_plt = not debug
+        # region = self.regions[key]
 
-        region = self.regions[key]
-
-        covid_df = self.data_df_dict[region]
         sma_win = self.sma_win
         ewma_spn = self.ewma_spn
 
-        # Plot #1 -- Total cases
-        if do_total:
-            if do_raw:
-                plot_totals((covid_df['Date'], covid_df['Total Cases']),
-                            region, 'Totals',
-                            'Total Confirmed COVID positive cases',
-                            save_plt)
+        # All New Cases Scaled by SMA
+        def all_cases_scaled_sma(ax):
+            us_df = self.data_df_dict[self.regions['US']]
+            nj_df = self.data_df_dict[self.regions['NJ']]
+            ct_df = self.data_df_dict[self.regions['Counties']]
+            bc_df = self.data_df_dict[self.regions['Bergen']]
+            rf_df = self.data_df_dict[self.regions['Rutherford']]
 
-        # Plot -- Raw & smoothed cases
-        if do_avgs:
-            if do_raw:
-                y_data_list = [covid_df['New Cases'],
-                               covid_df[str(sma_win) + 'd avg'],
-                               covid_df[str(ewma_spn) + 'd ewma']]
-                plot_avgs((covid_df['Date'], y_data_list),
-                          region, 'Confirmed',
-                          'Confirmed COVID positive cases',
-                          save_plt)
+            ax.plot(us_df['Date'], us_df[str(sma_win) + 'd avg / 100K'])
+            ax.plot(nj_df['Date'], nj_df[str(sma_win) + 'd avg / 100K'])
+            ax.plot(ct_df['Date'], ct_df[str(sma_win) + 'd avg / 100K'])
+            ax.plot(bc_df['Date'], bc_df[str(sma_win) + 'd avg / 100K'])
+            ax.plot(rf_df['Date'], rf_df[str(sma_win) + 'd avg / 100K'])
 
-            if do_scaled:
-                # Plot -- Raw & smoothed cases / 100K people
-                y_data_list = [covid_df['New Cases / 100K'],
-                               covid_df[str(sma_win) + 'd avg / 100K'],
-                               covid_df[str(ewma_spn) + 'd ewma / 100K']]
-                plot_avgs((covid_df['Date'], y_data_list),
-                          region, 'Confirmed_per100K',
-                          'Confirmed COVID positive cases per 100K population',
-                          save_plt)
+            return ax
+
+        plot_config = {'plot_fn': all_cases_scaled_sma,
+                       'fname': 'Totals_' + str(sma_win) + 'D_SMA_per_100K',
+                       'title': str(sma_win) + ' day average / 100K residents',
+                       'legend': ['United States',
+                                  'New Jersey',
+                                  'Other Counties',
+                                  'Bergen County',
+                                  'Rutherford']
+                       }
+        self.make_plot(plot_config)
+
+        # All New Cases Scaled by EWMA
+        def all_cases_scaled_ewma(ax):
+            us_df = self.data_df_dict[self.regions['US']]
+            nj_df = self.data_df_dict[self.regions['NJ']]
+            ct_df = self.data_df_dict[self.regions['Counties']]
+            bc_df = self.data_df_dict[self.regions['Bergen']]
+            rf_df = self.data_df_dict[self.regions['Rutherford']]
+
+            ax.plot(us_df['Date'], us_df[str(ewma_spn) + 'd ewma / 100K'])
+            ax.plot(nj_df['Date'], nj_df[str(ewma_spn) + 'd ewma / 100K'])
+            ax.plot(ct_df['Date'], ct_df[str(ewma_spn) + 'd ewma / 100K'])
+            ax.plot(bc_df['Date'], bc_df[str(ewma_spn) + 'd ewma / 100K'])
+            ax.plot(rf_df['Date'], rf_df[str(ewma_spn) + 'd ewma / 100K'])
+
+            return ax
+
+        plot_config = {'plot_fn': all_cases_scaled_ewma,
+                       'fname': 'Totals_' + str(ewma_spn) + 'D_EWMA_per_100K',
+                       'title': str(ewma_spn) + ' day weighted average / 100K residents',
+                       'legend': ['United States',
+                                  'New Jersey',
+                                  'Other Counties',
+                                  'Bergen County',
+                                  'Rutherford']
+                       }
+        self.make_plot(plot_config)
+
+        # Total New Cases Scaled
+        def total_cases_scaled(ax):
+            us_df = self.data_df_dict[self.regions['US']]
+            nj_df = self.data_df_dict[self.regions['NJ']]
+            ct_df = self.data_df_dict[self.regions['Counties']]
+            bc_df = self.data_df_dict[self.regions['Bergen']]
+            rf_df = self.data_df_dict[self.regions['Rutherford']]
+
+            ax.plot(us_df['Date'], us_df['Total Cases / 100K'])
+            ax.plot(nj_df['Date'], nj_df['Total Cases / 100K'])
+            ax.plot(ct_df['Date'], ct_df['Total Cases / 100K'])
+            ax.plot(bc_df['Date'], bc_df['Total Cases / 100K'])
+            ax.plot(rf_df['Date'], rf_df['Total Cases / 100K'])
+
+            return ax
+        plot_config = {'plot_fn': total_cases_scaled,
+                       'fname': 'Totals_cases_per_100K',
+                       'title': 'Total cases / 100K residents',
+                       'legend': ['United States',
+                                  'New Jersey',
+                                  'Other Counties',
+                                  'Bergen County',
+                                  'Rutherford']
+                       }
+        self.make_plot(plot_config)
+
+        # Total New Cases Scaled by SMA
+        def total_cases_scaled_sma(ax):
+            us_df = self.data_df_dict[self.regions['US']]
+            nj_df = self.data_df_dict[self.regions['NJ']]
+            ct_df = self.data_df_dict[self.regions['Counties']]
+            bc_df = self.data_df_dict[self.regions['Bergen']]
+            rf_df = self.data_df_dict[self.regions['Rutherford']]
+
+            ax.plot(us_df['Date'], us_df[str(sma_win) + 'd avg Total Cases / 100K'])
+            ax.plot(nj_df['Date'], nj_df[str(sma_win) + 'd avg Total Cases / 100K'])
+            ax.plot(ct_df['Date'], ct_df[str(sma_win) + 'd avg Total Cases / 100K'])
+            ax.plot(bc_df['Date'], bc_df[str(sma_win) + 'd avg Total Cases / 100K'])
+            ax.plot(rf_df['Date'], rf_df[str(sma_win) + 'd avg Total Cases / 100K'])
+
+            return ax
+        plot_config = {'plot_fn': total_cases_scaled_sma,
+                       'fname': 'Totals_cases_per_100K_SMA',
+                       'title': 'Total cases / 100K residents -- ' + str(sma_win) + ' day average',
+                       'legend': ['United States',
+                                  'New Jersey',
+                                  'Other Counties',
+                                  'Bergen County',
+                                  'Rutherford']
+                       }
+        self.make_plot(plot_config)
+
+        #
+        # # Plot #1 -- Total cases
+        # if do_total:
+        #     if do_raw:
+        #         plot_config = {'x_col': 'Date',
+        #                        'y_cols': ['Total Cases'],
+        #                        'plot_args': [{}],
+        #                        'fname': 'Totals',
+        #                        'title': 'Total Confirmed COVID positive cases',
+        #                        'region': region,
+        #                        'legend': False
+        #                        }
+        #         self.make_plot(plot_config, save_plt)
+        #
+        # # Plot -- Raw & smoothed cases
+        # if do_avgs:
+        #     plot_config = {'x_col': 'Date',
+        #                    'plot_args': [{'color': 'gray', 'marker': 'o', 'fillstyle': 'none', 'ms': 2, 'lw': 0},
+        #                                  {'color': 'blue'},
+        #                                  {'color': 'orange'}],
+        #                    'region': region,
+        #                    'legend': True
+        #                    }
+        #     if do_raw:
+        #         plot_config['y_cols'] = ['New Cases', str(sma_win) + 'd avg', str(ewma_spn) + 'd ewma']
+        #         plot_config['fname'] = 'Confirmed'
+        #         plot_config['title'] = 'Confirmed COVID positive cases'
+        #         self.make_plot(plot_config, save_plt)
+        #
+        #     if do_scaled:
+        #         # Plot -- Raw & smoothed cases / 100K people
+        #         plot_config['y_cols'] = ['New Cases / 100K', str(sma_win) + 'd avg / 100K',
+        #                                  str(ewma_spn) + 'd ewma / 100K']
+        #         plot_config['fname'] = 'Confirmed_per100K'
+        #         plot_config['title'] = 'Confirmed COVID positive cases per 100K population'
+        #         self.make_plot(plot_config, save_plt)
 
     def do_tables(self, key, config):
         """ Print some tabular information of stats
 
-        :param covid_data: covid data class
-        :param region: region to plot
-        :param save_stats: save to a file(T) or show to screen(F)
+        :param key: region to plot
+        :param config: save to a file(T) or show to screen(F)
         """
         debug = config.get('debug', True)
-
-        save_stats = not debug
 
         region = self.regions[key]
 
@@ -260,11 +306,46 @@ class CovidData:
 
         smallest = covid_df['7d avg'].nsmallest(10, keep='first')
 
-        if save_stats:
+        if self.debug:
+            print(smallest)
+        else:
             pass
             # smallest.to_csv('smallest.csv', sep=',', mode='w')
+
+    def make_plot(self, config):
+        """ Plot town cases and averages
+
+        :param config: dict of
+                - x_col: x columns (str)
+                - y_cols: list of y columns
+                - plot_args: list of args for plot
+                - region: area we are plotting
+                - fname: filename suffux
+                - title: chart title (str)
+        :param save_plt: save to a file(T) or show to screen(F)
+        """
+
+        fig = plt.figure(figsize=(13, 9))
+        ax = fig.add_subplot(1, 1, 1)
+
+        config['plot_fn'](ax)
+
+        ax.set_ylabel('# of cases', fontsize=20)
+        my_fmt = mdates.DateFormatter('%m/%d')
+        ax.xaxis.set_major_formatter(my_fmt)
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+
+        plt.title(config['title'], fontsize=24)
+        if config.get('legend', None) is not None:
+            plt.legend(config['legend'], fontsize=16)
+        plt.grid()
+
+        if self.debug:
+            plt.show()
         else:
-            print(smallest)
+            plt.savefig(config['fname'] + ".svg", format="svg")
+            plt.close()
 
 
 def main():
@@ -273,8 +354,8 @@ def main():
     """
 
     # Init covid class
-    sma_win = 7
-    ewma_span = 14.0
+    sma_win = 14
+    ewma_span = 14 # spn=29, com=14 | spn=15, com=7
     data_config = {'sma_win': sma_win,
                    'ewma_spn': ewma_span}
 
@@ -283,19 +364,14 @@ def main():
     # Dir to write output to (assuming root is where this script is)
     os.chdir('docs')
 
-    # plot Rutherford local data
+    covid_data.do_plots(key=None, config={'debug': False})
+
     # plot_config = {'raw': True,
     #                'scaled': True,
     #                'debug': False}
-    # region = covid_data.regions['Rutherford']
-    # covid_data.do_plots(key=region, config=plot_config)
-
-    plot_config = {'raw': True,
-                   'scaled': True,
-                   'debug': False}
-    for region in covid_data.regions:
-        covid_data.do_plots(key=region, config=plot_config)
-        covid_data.do_tables(key=region, config=plot_config)
+    # for region in covid_data.regions:
+    #     covid_data.do_plots(key=region, config=plot_config)
+    #     covid_data.do_tables(key=region, config=plot_config)
 
 
 if __name__ == "__main__":
