1800ee78c62922a7578e4ee0076b9d418bec3098
James Bogosian <bogosj@users.noreply.github.com>
2020-11-10 21:27:08 -0500

    Update update_data.sh

--- .github/workflows/update_data.sh
+++ .github/workflows/update_data.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-cd data && go run *.go && ../
+cd data && go run *.go && cd ../
 git config user.name github-actions
 git config user.email github-actions@github.com
 timestamp=$(TZ=America/New_York date)
