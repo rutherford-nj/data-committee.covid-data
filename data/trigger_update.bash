9137cefa6aef46d4cd9bd6ad05b1adf7012c0ca4
James Bogosian <bogosj@users.noreply.github.com>
2020-11-20 11:01:33 -0500

    Delete trigger_update.bash

--- data/trigger_update.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z ${GITHUB_PAT+x} ]; then echo "Set GITHUB_PAT before running."; exit 1; fi
-
-curl -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token $GITHUB_PAT" \
-    --request POST \
-    --data '{"event_type": "update-data"}' \
-    https://api.github.com/repos/grecine/covid_data/dispatches
