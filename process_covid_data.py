69a78f7102c5a746a78d89037fcbc62198d9fba3
grecine <grecine@gmail.com>
2020-11-24 13:04:43 -0500

    test 2

--- process_covid_data.py
+++ process_covid_data.py
@@ -9,7 +9,7 @@ import os
 
 from lib import covid
 
-# testing comment for git
+# testing comment for git --- again
 MakeThesePlots = [
     covid.MakePlots.plot_new_cases_scaled_sma,       # All New Cases Scaled by SMA
     # covid.MakePlots.plot_new_cases_scaled_ewma,    # All New Cases Scaled by EWMA
