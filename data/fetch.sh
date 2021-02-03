245669204569a249cbe4e2cf755971cfaca6ee3b
James Bogosian <bogosj@users.noreply.github.com>
2021-02-03 09:28:16 -0500

    Stop fetching COVID Tracking Project (#5)
    * Update fetch.sh
    
    * Update Dockerfile

--- data/fetch.sh
+++ data/fetch.sh
@@ -2,22 +2,6 @@
 
 cd /work
 
-echo "Date,New Cases,Total Cases" > data/csv/covid_tracking_us.csv
-curl https://api.covidtracking.com/v1/us/daily.json | \
-  jq -r '.[] | {d: .date|tostring, p: .positive, pi: .positiveIncrease} |
-      select(.p != null) | select(.pi != null) |
-      {d: (.d[:4] + "-" + .d[4:6] + "-" + .d[6:]), p: .p, pi: .pi} |
-      [.d, .pi, .p] | @csv' \
-  >> data/csv/covid_tracking_us.csv
-
-echo "Date,New Cases,Total Cases" > data/csv/covid_tracking_nj.csv
-curl https://api.covidtracking.com/v1/states/nj/daily.json | \
-  jq -r '.[] | {d: .date|tostring, p: .positive, pi: .positiveIncrease} |
-      select(.p != null) | select(.pi != null) |
-      {d: (.d[:4] + "-" + .d[4:6] + "-" + .d[6:]), p: .p, pi: .pi} |
-      [.d, .pi, .p] | @csv' \
-  >> data/csv/covid_tracking_nj.csv
-
 curl https://docs.google.com/spreadsheets/d/e/2PACX-1vS00GBGJKB0Xwtru3Rn5WrPqur19j--CibdM5R1tbnis0W_Bp18EmLFkJJc5sG4dwvMyqCorSVhHwik/pub?output=csv \
   -L --output data/csv/rutherford_data.csv
 
