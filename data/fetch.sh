d14c2c01a7be6a2e564a4104f6b683a23d3fc57d
James Bogosian <bogosj@users.noreply.github.com>
2021-02-03 09:31:13 -0500

    Update fetch.sh

--- data/fetch.sh
+++ data/fetch.sh
@@ -5,9 +5,8 @@ cd /work
 curl https://docs.google.com/spreadsheets/d/e/2PACX-1vS00GBGJKB0Xwtru3Rn5WrPqur19j--CibdM5R1tbnis0W_Bp18EmLFkJJc5sG4dwvMyqCorSVhHwik/pub?output=csv \
   -L --output data/csv/rutherford_data.csv
 
-
 #############################
-# # Get data from NY Times
+## Get data from NY Times
 
 # All counties in NJ
 echo "Date,County,State,FIPS,Total Cases,Total Deaths" > data/csv/nytimes_nj_counties.csv
