cfee14193f3c355606bfa4d2d17361bd79c4a6c6
grecine <grecine@gmail.com>
2021-02-17 23:42:44 -0500

    run daily snapshot

--- process_covid_data.py
+++ process_covid_data.py
@@ -10,21 +10,22 @@ import os
 from lib import covid
 
 MakeThesePlots = [
-    covid.MakePlots.new_cases_norm_sma,        # All New Cases Scaled by SMA
-    # covid.MakePlots.new_cases_norm_ewma,       # All New Cases Scaled by EWMA
-    # covid.MakePlots.total_cases_norm,          # Total New Cases Scaled
-    covid.MakePlots.total_cases_norm_sma,      # Total New Cases Scaled by SMA
-    covid.MakePlots.rutherford_new_cases,      # Rutherford new cases, unscaled
-    covid.MakePlots.rutherford_new_cases_reg,      # Rutherford new cases, unscaled & fitted
-    covid.MakePlots.rutherford_total_cases,    # Rutherford total cases, unscaled
-    covid.MakePlots.new_cases_norm_sma_wow,    # Rutherford new week-over-week cases, unscaled
-    covid.MakePlots.new_cases_norm_sma_slope,  # Rutherford X day slope of scaled, SMA new cases
-    covid.MakePlots.incidence,                 # Total New Cases scaled 14d sum ("Incidence")
-    covid.MakePlots.trajectory,                # sma incidence slope
+    covid.MakePlots.new_cases_norm_sma,         # All New Cases Scaled by SMA
+    # covid.MakePlots.new_cases_norm_ewma,        # All New Cases Scaled by EWMA
+    # covid.MakePlots.total_cases_norm,           # Total New Cases Scaled
+    covid.MakePlots.total_cases_norm_sma,       # Total New Cases Scaled by SMA
+    covid.MakePlots.rutherford_new_cases,       # Rutherford new cases, unscaled
+    covid.MakePlots.rutherford_new_cases_reg,   # Rutherford new cases, unscaled & fitted
+    covid.MakePlots.rutherford_total_cases,     # Rutherford total cases, unscaled
+    covid.MakePlots.new_cases_norm_sma_wow,     # Rutherford new week-over-week cases, unscaled
+    covid.MakePlots.new_cases_norm_sma_slope,   # Rutherford X day slope of scaled, SMA new cases
+    covid.MakePlots.incidence,                  # Total New Cases scaled 14d sum ("Incidence")
+    covid.MakePlots.trajectory,                 # sma incidence slope
 ]
 
 CalcTheseStats = [
-    covid.MakeStats.rutherford_cases,      # Some stat type goes here
+    covid.MakeStats.rutherford_cases,           # Total, New, and SMA cases for town
+    covid.MakeStats.today_snapshot,             # Today's numbers
 ]
 
 
