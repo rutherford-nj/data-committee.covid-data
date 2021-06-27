4c27b6911ca00558d8903a4f8878dbb8ea5c95f7
James Bogosian <bogosj@gmail.com>
2021-06-27 12:47:38 -0400

    Fix logic so the prior days cases don't copy over.

--- .github/workflows/zero_rutherford.bash
+++ .github/workflows/zero_rutherford.bash
@@ -3,12 +3,12 @@
 currenttime=$(TZ=America/New_York date +%H:%M)
 if [[ "$currenttime" > "21:00" ]]; then
   today=$(TZ=America/New_York date +%-m/%-d/%Y)
-  prev_cases=$(tail -n 1 ./data/csv/rutherford_data.csv | cut -d"," -f2-)
+  prev_cases=$(tail -n 1 ./data/csv/rutherford_data.csv | cut -d"," -f2)
   if grep -q $today ./data/csv/rutherford_data.csv; then
     echo "today's data already recorded"
   else
     echo "no cases reported today, recording 0"
-    echo $today,$prev_cases >> ./data/csv/rutherford_data.csv
+    echo $today,$prev_cases,0 >> ./data/csv/rutherford_data.csv
   fi
 else
   echo "still too early in the day to assume no cases"
