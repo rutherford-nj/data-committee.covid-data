94e7b345ab8af8f040f6b1d26bc8598e6ab6072f
James Bogosian <bogosj@gmail.com>
2021-06-27 12:46:31 -0400

    Refactor rutherford zero cases logic to a new file.

--- .github/workflows/update_data.bash
+++ .github/workflows/update_data.bash
@@ -10,19 +10,7 @@ git config --global user.name github-actions
 git config --global user.email github-actions@github.com
 
 # Update Rutherford data with zero cases unless cases were recorded before 9pm.
-currenttime=$(TZ=America/New_York date +%H:%M)
-if [[ "$currenttime" > "21:00" ]]; then
-  today=$(TZ=America/New_York date +%-m/%-d/%Y)
-  prev_cases=$(tail -n 1 ./data/csv/rutherford_data.csv | cut -d"," -f2-)
-  if grep -q $today ./data/csv/rutherford_data.csv; then
-    echo "today's data already recorded"
-  else
-    echo "no cases reported today, recording 0"
-    echo $today,$prev_cases >> ./data/csv/rutherford_data.csv
-  fi
-else
-  echo "still too early in the day to assume no cases"
-fi
+.github/workflows/zero_rutherford.bash
 
 # Commit data changes. For scheduled runs, bail out of the workflow if there hasn't been a change in the data.
 timestamp=$(TZ=America/New_York date)
---
+++ .github/workflows/zero_rutherford.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+currenttime=$(TZ=America/New_York date +%H:%M)
+if [[ "$currenttime" > "21:00" ]]; then
+  today=$(TZ=America/New_York date +%-m/%-d/%Y)
+  prev_cases=$(tail -n 1 ./data/csv/rutherford_data.csv | cut -d"," -f2-)
+  if grep -q $today ./data/csv/rutherford_data.csv; then
+    echo "today's data already recorded"
+  else
+    echo "no cases reported today, recording 0"
+    echo $today,$prev_cases >> ./data/csv/rutherford_data.csv
+  fi
+else
+  echo "still too early in the day to assume no cases"
+fi
