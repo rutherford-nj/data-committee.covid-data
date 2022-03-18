8ba9a9dfbd9751d6c5271632eff82e8b309f8f76
James Bogosian <bogosj@users.noreply.github.com>
2022-03-18 22:22:33 +0000

    Backfill zero data when entering new data.

--- data/update_rutherford.py
+++ data/update_rutherford.py
@@ -3,11 +3,19 @@
 import datetime
 import sys
 
-with open('csv/rutherford_data.csv') as f:
+_CSV_PATH = 'csv/rutherford_data.csv'
+_TIME_FORMAT = '%-m/%-d/%Y'
+
+with open(_CSV_PATH) as f:
     lines = f.readlines()
     current = int(lines[-1].split(',')[1])
+    last_updated_date = datetime.datetime.strptime(lines[-1].split(',')[0], '%m/%d/%Y')
     new = int(sys.argv[1])
-    today = datetime.datetime.now().strftime('%-m/%-d/%Y')
+    today = datetime.datetime.now().strftime(_TIME_FORMAT)
     new_line = (f'{today},{new+current},{new}\n')
-    with open('csv/rutherford_data.csv', 'a') as w:
+    with open(_CSV_PATH, 'a') as w:
+        last_updated_date += datetime.timedelta(days=1)
+        while last_updated_date.strftime(_TIME_FORMAT) != today:
+            w.write(f'{last_updated_date.strftime(_TIME_FORMAT)},{current},0\n')
+            last_updated_date += datetime.timedelta(days=1)
         w.write(new_line)
